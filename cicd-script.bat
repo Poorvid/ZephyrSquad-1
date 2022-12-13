cbdc694c7fe22b61067a2ec6d336de5e941832ca
Poorvid <poorvid61@gmail.com>
2022-12-13 14:46:26 +0530

    commit 11

--- cicd-script.bat
+++ cicd-script.bat
@@ -53,7 +53,7 @@ set "$appendDateTimeInFolderName=true"
 set "$assigneeUser=5eeb37164784510aca76ca61"
 
 :: Fully-qualitified name of the test result file
-set "$resultPath=@\"./test-output/testng-results.xml/"
+set "$resultPath=@\".\test-output\testng-results.xml\"
 
 :: =========================================
 ::  Create an automation task, run it, send test results to Zephyr.
